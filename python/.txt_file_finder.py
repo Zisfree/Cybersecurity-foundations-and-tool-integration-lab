064c1fc507df31cbab5cb5fd40ff392129c09dac
Zisfree <ajoshi2134@gmail.com>
2026-06-23 00:28:51 +0530

    Update .txt_file_finder.py

--- python/.txt_file_finder.py
+++ python/.txt_file_finder.py
@@ -1,11 +1,12 @@
 import os
 
-def smell(etc):
+def smell(etc):                                                             # This etc might seem to have no use but it acts as a dynamic folder. Without it we cannot run search with ("/").
+    
     x = os.listdir(etc)
     text_files = 0
 
     for file in x:
-        if file.endswith(".txt"):
+        if file.endswith(".txt"):                                            # .endswith() as its name suggests is a tool to find what u tell it to which the file ends with.
             print(file)
             text_files = text_files + 1
             break
