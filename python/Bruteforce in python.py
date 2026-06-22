47ffd72f39b107554a31f25a7ae9e6757b1fa746
Zisfree <ajoshi2134@gmail.com>
2026-06-22 23:05:50 +0530

    Update Bruteforce in python.py

--- python/Bruteforce in python.py
+++ python/Bruteforce in python.py
@@ -3,6 +3,7 @@ attempts = 3
 
 while attempts > 0:                                     # While loop tells python to keep running the command. The attempts > 0 tell it to do it till the attempts left are 0.
     guess = input("Password of 5 digits:")
+    
 
     if guess == code:                                   # Here the if command analizes that the code matched with our guess or not.
        print("Correct!")
