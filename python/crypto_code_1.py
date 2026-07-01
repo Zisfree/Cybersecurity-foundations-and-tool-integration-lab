afcf4d969a9d7319d8053ecc05f831bfc5ee1b00
Zisfree <ajoshi2134@gmail.com>
2026-07-02 02:18:26 +0530

    Create crypto_code_1.py

---
+++ python/crypto_code_1.py
@@ -0,0 +1,7 @@
+import sys
+
+ords = [61,36,63,38,62,36,37,33,33,63,39,62,32,32,64,65,37,62,63,30,32,35,33,32,36,36,61,33,38,36,37,64,66,35,35,61,63,64,65,38,36,33,35,65,31,39,63,37,33,33,31,33]
+
+print("".join(chr(o) for o in ords))
+# chr() is used to convert the integers above to ASCII charecters
+# ord() function does opposite of chr()
